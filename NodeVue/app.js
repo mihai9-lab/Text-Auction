56cc8b176e511658e37ea133ed50a81e9e3c7bfc
mihailo.mi <mihailo.sky@gmail.com>
2021-09-17 21:49:55 +0200

    fix vue routing

--- NodeVue/app.js
+++ NodeVue/app.js
@@ -14,19 +14,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/api', router);
-
 const staticMiddleware = express.static(path.join(__dirname, 'dist'));
 app.use(staticMiddleware);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'dist','index.html'));
+});
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.status(err.status || 500).send();
+  res.status(err.status || 500).send();
 });
+
 new CronJob('0 0 0 * * *', async () => {
     const users = await models.user.findAll();
     users.forEach((user) => {
